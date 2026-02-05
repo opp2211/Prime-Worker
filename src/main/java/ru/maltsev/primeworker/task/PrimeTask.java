e6759c66dde07cabefa1483c17e898b74004ccfa
Kirill Maltsev <opp.2211@gmail.com>
2026-02-05 06:01:27 +0500

    Add cron task

---
+++ src/main/java/ru/maltsev/primeworker/task/PrimeTask.java
@@ -0,0 +1,18 @@
+package ru.maltsev.primeworker.task;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+import ru.maltsev.primeworker.service.PrimeService;
+
+@Service
+@RequiredArgsConstructor
+public class PrimeTask {
+
+    private final PrimeService primeService;
+
+    @Scheduled(cron = "0 0 * * * *")
+    public void doTask() {
+        primeService.updateSpreadsheetData();
+    }
+}
