213e8981826d629f8ac0b52d347c5d65f28f31dd
Kirill Maltsev <opp.2211@gmail.com>
2026-03-07 14:55:33 +0500

    feat(prime): добавить расчет курсов KZT и вернуть J3/J4 как BigDecimal

--- src/main/java/ru/maltsev/primeworker/service/PrimeService.java
+++ src/main/java/ru/maltsev/primeworker/service/PrimeService.java
@@ -25,13 +25,24 @@ public class PrimeService {
     public void updateSpreadsheetData() {
         List<String> rates = new ArrayList<>();
 
-        rates.add(getJ3Rate());
-        rates.add(getJ4Rate());
+        var usdToRubRate = getUsdToRubRate();
+        var rubToUsdRate = getRubToUsdRate();
+
+        var kztToUsdRate = getKztToUsdRate();
+        var kztToRubRate = getKztToRubRate(kztToUsdRate, usdToRubRate);
+
+        var usdToKztRate = getUsdToKztRate();
+        var rubToKztRate = getRubToKztRate(rubToUsdRate, usdToKztRate);
+
+        rates.add(formatDecimalToCommaString(usdToRubRate));
+        rates.add(formatDecimalToCommaString(rubToUsdRate));
+        rates.add(formatDecimalToCommaString(kztToRubRate));
+        rates.add(formatDecimalToCommaString(rubToKztRate));
 
         ratesSheet.updateRates(rates);
     }
 
-    private String getJ3Rate() {
+    private BigDecimal getUsdToRubRate() {
         BybitAd ad = bybitService.getSingleAd(BybitToken.USDT,
                 BybitFiat.RUB,
                 BybitSide.BUY,
@@ -39,10 +50,11 @@ public class PrimeService {
                 10_000,
                 8);
         double rate = ad.getPrice() / 1.00275;
-        return formatDoubleToCommaString(rate);
+        return BigDecimal.valueOf(rate)
+                .setScale(4, RoundingMode.DOWN);
     }
 
-    private String getJ4Rate() {
+    private BigDecimal getRubToUsdRate() {
         BybitAd ad = bybitService.getSingleAd(BybitToken.USDT,
                 BybitFiat.RUB,
                 BybitSide.BUY,
@@ -51,15 +63,40 @@ public class PrimeService {
                 20);
 
         double invertedPrice = 1 / ad.getPrice();
-
-        BigDecimal decimal = new BigDecimal(invertedPrice)
+        return BigDecimal.valueOf(invertedPrice)
                 .setScale(4, RoundingMode.DOWN);
-
-        return formatDecimalToCommaString(decimal);
     }
 
-    private String formatDoubleToCommaString(double value) {
-        return String.valueOf(value).replace('.', ',');
+    private BigDecimal getUsdToKztRate() {
+        BybitAd ad = bybitService.getSingleAd(BybitToken.USDT,
+                BybitFiat.KZT,
+                BybitSide.BUY,
+                List.of(BybitPaymentMethod.KASPI_BANK),
+                50_000,
+                2);
+
+        return BigDecimal.valueOf(ad.getPrice())
+                .setScale(2, RoundingMode.DOWN);
+    }
+
+    private BigDecimal getKztToUsdRate() {
+        BybitAd ad = bybitService.getSingleAd(BybitToken.USDT,
+                BybitFiat.KZT,
+                BybitSide.BUY,
+                List.of(BybitPaymentMethod.KASPI_BANK),
+                50_000,
+                4);
+
+        return BigDecimal.valueOf(1 / ad.getPrice())
+                .setScale(4, RoundingMode.DOWN);
+    }
+
+    private BigDecimal getKztToRubRate(BigDecimal kztToUsdRate, BigDecimal usdToRubRate) {
+        return kztToUsdRate.multiply(usdToRubRate);
+    }
+
+    private BigDecimal getRubToKztRate(BigDecimal rubToUsdRate, BigDecimal usdToKztRate) {
+        return rubToUsdRate.multiply(usdToKztRate);
     }
 
     private String formatDecimalToCommaString(BigDecimal value) {
