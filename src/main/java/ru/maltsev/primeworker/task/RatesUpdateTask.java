cd252cf556ff04566540fb9ccc96acc161223790
Kirill Maltsev <opp.2211@gmail.com>
2026-03-09 15:42:37 +0500

    chore(scheduler): увеличить частоту обновления до каждых 2 минут

--- src/main/java/ru/maltsev/primeworker/task/RatesUpdateTask.java
+++ src/main/java/ru/maltsev/primeworker/task/RatesUpdateTask.java
@@ -11,7 +11,7 @@ public class RatesUpdateTask {
 
     private final RatesUpdateService ratesUpdateService;
 
-    @Scheduled(cron = "0 0/5 * * * *")
+    @Scheduled(cron = "0 0/2 * * * *")
     public void doTask() {
         ratesUpdateService.updateRatesSheet();
     }
